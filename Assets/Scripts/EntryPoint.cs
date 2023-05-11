45c95fa4aa993a2dda6020ffbdde8e259a55e189
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-05-12 00:34:28 +0400

    Added entry point

---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset _input;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private List<ModelData> _modelDatas;
+
+    private Model _model;
+
+    private void Awake()
+    {
+        InputActionMap inputMap = _input.FindActionMap("Input");
+        
+        Menu menu = new (inputMap, _canvas);
+        _model = new Model(_modelDatas, inputMap, Camera.main);
+    }
+
+    private void Update()
+    {
+        _model.Update();
+    }
+}
