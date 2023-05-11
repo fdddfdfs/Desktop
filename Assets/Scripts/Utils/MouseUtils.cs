92f4b74a8652ec3e3fc237cf85a87a5c9cd67c21
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-05-12 00:34:18 +0400

    Added utils

---
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public static class MouseUtils
+{
+    public static Vector2 GetMousePosition()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition -= new Vector2(Screen.width, Screen.height) / 2;
+
+        return mousePosition;
+    }
+}
