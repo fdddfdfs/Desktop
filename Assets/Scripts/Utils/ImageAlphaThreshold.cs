c348463f6f6d2cc0217e29a5ee5c7dbf9f29aff8
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-05-13 20:52:11 +0400

    Removed transparent pixels raycasting

---
+++ Assets/Scripts/Utils/ImageAlphaThreshold.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageAlphaThreshold : MonoBehaviour
+{
+    private const float AlphaThreshold = 1f;
+    
+    [SerializeField] private Image _image;
+
+    private void Awake()
+    {
+        if (!_image)
+        {
+            _image = GetComponent<Image>();
+
+            if (!_image)
+            {
+                throw new Exception("Cannot find Image component");
+            }
+        }
+
+        _image.alphaHitTestMinimumThreshold = AlphaThreshold;
+    }
+}
