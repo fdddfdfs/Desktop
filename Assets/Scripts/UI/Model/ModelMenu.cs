10ebe0d06b8f880f441ae255ce2cb365bd0925b2
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-05-14 17:16:02 +0400

    Added models menu

---
+++ Assets/Scripts/UI/Model/ModelMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelMenu : IMenu
+{
+    private const string ModelMenuViewResourceName = "UI/ModelMenuView";
+
+    private readonly ModelMenuView _modelMenuView;
+    private readonly Model _model;
+    private readonly List<ModelData> _modelDatas;
+
+    private int _currentModelIndex;
+    
+    public ModelMenu(Model model, List<ModelData> modelDatas, Canvas canvas)
+    {
+        _model = model;
+        _modelDatas = modelDatas;
+        _modelMenuView = ResourcesLoader.InstantiateLoadComponent<ModelMenuView>(ModelMenuViewResourceName);
+        _modelMenuView.transform.SetParent(canvas.transform, false);
+        _modelMenuView.Init(this);
+        
+        for (int i = 1; i < _modelDatas.Count; i++)
+        {
+            _modelMenuView.ChangeButtonSprite(i - 1, _modelDatas[i].Icon);
+        }
+    }
+
+    public void ChangeModel(int index)
+    {
+        if (index >= _currentModelIndex)
+        {
+            index += 1;
+        }
+        
+        _model.ChangeModel(_modelDatas[index]);
+
+        int startIndex = Mathf.Min(index, _currentModelIndex);
+        int currentIndex = startIndex;
+        for (int i = startIndex; i < _modelDatas.Count; i++)
+        {
+            if (i == index) continue;
+            _modelMenuView.ChangeButtonSprite(currentIndex, _modelDatas[i].Icon);
+            currentIndex += 1;
+        }
+        
+        _currentModelIndex = index;
+    }
+
+    public void ChangeMenuActive(Vector2 position)
+    {
+        _modelMenuView.ChangeMenuActive(position);
+    }
+
+    public void HideMenu()
+    {
+        _modelMenuView.HideIfActive();
+    }
+}
---
+++ Assets/Scripts/UI/Model/ModelMenuView.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModelMenuView : MonoBehaviour
+{
+    private const float TargetRotationZ = 45f;
+    private const float Duration = 0.25f;
+    
+    [SerializeField] private GameObject _menu;
+    [SerializeField] private List<Button> _buttons;
+    [SerializeField] private List<Image> _buttonsImages;
+    
+    private bool _isActive;
+
+    public void Init(ModelMenu modelMenu)
+    {
+        for (var i = 0; i < _buttons.Count; i++)
+        {
+            int index = i;
+            _buttons[i].onClick.AddListener(()=> modelMenu.ChangeModel(index));
+        }
+        
+        _menu.SetActive(false);
+    }
+
+    public void ChangeButtonSprite(int index, Sprite sprite)
+    {
+        _buttonsImages[index].sprite = sprite;
+    }
+
+    public void ChangeMenuActive(Vector2 position)
+    {
+        if (!_isActive)
+        {
+            ShowMenu(position);
+        }
+        else
+        {
+            HideMenu();
+        }
+    }
+
+    public void HideIfActive()
+    {
+        if (_isActive) HideMenu();
+    }
+
+    private void ShowMenu(Vector2 position)
+    {
+        _menu.transform.localPosition = position;
+        _menu.SetActive(true);
+        _isActive = true;
+        
+        _menu.transform.DOKill();
+        _menu.transform.DOScale(1, Duration / 2)
+            .OnComplete(() => _menu.transform.DOLocalRotate(new Vector3(0, 0, TargetRotationZ), Duration / 2));
+        //_menu.transform.DOLocalRotate(new Vector3(0, 0, TargetRotationZ), Duration);
+    }
+
+    private void HideMenu()
+    {
+        _isActive = false;
+        
+        _menu.transform.DOKill();
+        _menu.transform.DOScale(0, Duration);
+        _menu.transform.DOLocalRotate(Vector3.zero, Duration).OnComplete(() => _menu.SetActive(false));
+    }
+}
