fca8cc3a0c35fafe150c3d8c1cbace8c683fedfd
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-06-20 19:26:16 +0400

    Implement customization, updated animations for using UnityEngine instead of UnityEditor

--- Assets/Scripts/ModelView.cs
+++ Assets/Scripts/ModelView.cs
@@ -3,45 +3,108 @@ using UnityEngine;
 
 public class ModelView : MonoBehaviour
 {
+    private const string ResetTriggerName = "Reset";
+    
     [SerializeField] private GameObject _model;
     [SerializeField] private Animator _animator;
     [SerializeField] private BoxCollider _boxCollider;
-    [SerializeField] private MeshRenderer _mesh;
     [SerializeField] private List<CustomizationMesh> _customizationMeshes;
 
-    private Dictionary<CustomizationType, List<MeshRenderer>> _customizationMeshesByType;
+    private readonly int _resetTriggerHash = Animator.StringToHash(ResetTriggerName);
+
+    private AnimatorOverrideController _animatorOverrideController;
+    private string _defaultClipName;
+
+    private Dictionary<CustomizationType, List<CustomizationMesh>> _customizationMeshesByType;
 
     public Vector2 Size => _boxCollider.size * (Vector2)_boxCollider.transform.localScale;
+
+    public Transform ModelTransform => _model.transform;
+
+    public BoxCollider BoxCollider => _boxCollider;
 
     public void ChangeActive(bool active)
     {
         _model.SetActive(active);
     }
 
-    public void ChangeAnimation(RuntimeAnimatorController runtimeAnimatorController)
+    public void ChangeAnimation(AnimationClip animationClip)
     {
-        _animator.runtimeAnimatorController = runtimeAnimatorController;
+        _animatorOverrideController[_defaultClipName] = animationClip;
+        _animator.SetTrigger(_resetTriggerHash);
     }
 
     public void Customize(CustomizationType type, Material newMaterial)
     {
-        foreach (MeshRenderer meshRenderer in _customizationMeshesByType[type])
+        foreach (CustomizationMesh customizationMesh in _customizationMeshesByType[type])
         {
-            meshRenderer.material = newMaterial;
+            Material[] materials = customizationMesh.MeshRenderer.materials;
+            materials[customizationMesh.Index] = newMaterial;
+            customizationMesh.MeshRenderer.materials = materials;
+        }
+    }
+
+    public void CustomizeHairs(HairData hairData)
+    {
+        foreach (HairMaterial hairMaterial in hairData.HairMaterials)
+        {
+            if (!_customizationMeshesByType.ContainsKey(hairMaterial.CustomizationType)) continue;
+            
+            foreach (CustomizationMesh customizationMesh in _customizationMeshesByType[hairMaterial.CustomizationType])
+            {
+                Material[] materials = customizationMesh.MeshRenderer.materials;
+                materials[customizationMesh.Index] = hairMaterial.HairsMaterial;
+                customizationMesh.MeshRenderer.materials = materials;
+            }
+        }
+    }
+
+    public void CustomizeWeight(CustomizationType type, float newWeight)
+    {
+        foreach (CustomizationMesh customizationMesh in _customizationMeshesByType[type])
+        {
+            if (type == CustomizationType.TopBody)
+            {
+                if (newWeight > 0.5f)
+                {
+                    customizationMesh.MeshRenderer.SetBlendShapeWeight(
+                        customizationMesh.Index,
+                        (newWeight * 2 - 1) * 100);
+                    customizationMesh.MeshRenderer.SetBlendShapeWeight(
+                        customizationMesh.Index + 1, 0);
+                }
+                else
+                {
+                    customizationMesh.MeshRenderer.SetBlendShapeWeight(customizationMesh.Index, 0);
+                    customizationMesh.MeshRenderer.SetBlendShapeWeight(
+                        customizationMesh.Index + 1, 
+                        (1 - newWeight * 2) * 100);
+                }
+            }
+            else
+            {
+                customizationMesh.MeshRenderer.SetBlendShapeWeight(customizationMesh.Index, newWeight * 100);
+            }
         }
     }
 
     private void Awake()
     {
-        _customizationMeshesByType = new Dictionary<CustomizationType, List<MeshRenderer>>();
+        _customizationMeshesByType = new Dictionary<CustomizationType, List<CustomizationMesh>>();
         foreach (CustomizationMesh customizationMesh in _customizationMeshes)
         {
             if (!_customizationMeshesByType.ContainsKey(customizationMesh.CustomizationType))
             {
-                _customizationMeshesByType[customizationMesh.CustomizationType] = new List<MeshRenderer>();
+                _customizationMeshesByType[customizationMesh.CustomizationType] = new List<CustomizationMesh>();
             }
             
-            _customizationMeshesByType[customizationMesh.CustomizationType].Add(customizationMesh.MeshRenderer);
+            _customizationMeshesByType[customizationMesh.CustomizationType].Add(customizationMesh);
         }
+
+        _animatorOverrideController = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+        _animator.runtimeAnimatorController = _animatorOverrideController;
+        _defaultClipName = _animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        _animator.applyRootMotion = false;
     }
 }
